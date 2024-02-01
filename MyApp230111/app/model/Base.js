356908a2153e674706ee0090a09dac246bcdd221
DESKTOP-MTAR406\postmath21 <dhyeon1320@gmail.com>
2024-02-01 19:34:59 +0900

    gitignore 수정

--- MyApp230111/app/model/Base.js
+++
@@ -1,7 +0,0 @@
-Ext.define('MyApp230111.model.Base', {
-    extend: 'Ext.data.Model',
-
-    schema: {
-        namespace: 'MyApp230111.model'
-    }
-});
--- MyApp230111/app/model/Personnel.js
+++
@@ -1,7 +0,0 @@
-Ext.define('MyApp230111.model.Personnel', {
-    extend: 'MyApp230111.model.Base',
-
-    fields: [
-        'name', 'email', 'phone'
-    ]
-});
