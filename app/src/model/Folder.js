ab35faf5c85c233c2bbf1c09b482939eb62ed43c
DESKTOP-TQ1NH48\postmath21 <dhyeon94@gmail.com>
2024-02-01 19:07:52 +0900

    gitignore 수정

--- app/src/model/Folder.js
+++
@@ -1,9 +0,0 @@
-Ext.define('MyApp230111.model.Folder', {
-    extend: 'MyApp230111.model.Base',
-    fields: [
-        { name: 'id', type: 'number' },
-        { name: 'title', type: 'string' },
-        { name: 'total', type: 'number' },
-        { name: 'difficulty', type: 'number' }
-    ]
-})
